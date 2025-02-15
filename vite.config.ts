b233f916278d8c8b90910a8322f05941c0fff3df
Nikita Hramyka <ramich@tut.by>
2025-02-15 14:11:02 +0100

    Comment out base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/techtalentpro/',
   plugins: [react(), tailwindcss()],
-  base: "/main-project/",
+  // base: "/main-project/",
 });
