16afde40d95847a456758009d98b9066bf1db9a1
Eduard Brukish <eduardbrukish@gmail.com>
2025-03-23 18:27:50 +0200

    feat(ui-updates): add customer savings block

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -12,30 +12,38 @@ const AboutUsSection = () => {
     <Section sectionName={"about"}>
       <Container>
         <SectionHeader>{t("aboutOurCompany")}</SectionHeader>
-        <div className="grid md:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-4 gap-8">
           <motion.div
             className="text-center"
             transition={{ ease: "easeOut", duration: 1 }}
             {...commonFadeUpAnimationProps}
           >
-            <div className="text-4xl text-blue-600 mb-4">5+</div>
-            <p className="text-gray-600">{t("yearsInIt")}</p>
+            <div className="text-4xl text-primary-blue mb-4">5+</div>
+            <p className="text-color-text">{t("yearsInIt")}</p>
           </motion.div>
           <motion.div
             className="text-center"
             transition={{ ease: "easeOut", duration: 1, delay: 0.2 }}
             {...commonFadeUpAnimationProps}
           >
-            <div className="text-4xl text-blue-600 mb-4">100+</div>
-            <p className="text-gray-600">{t("fullTimeProfessionals")}</p>
+            <div className="text-4xl text-primary-blue mb-4">50+</div>
+            <p className="text-color-text">{t("fullTimeProfessionals")}</p>
           </motion.div>
           <motion.div
             className="text-center"
             transition={{ ease: "easeOut", duration: 1, delay: 0.4 }}
             {...commonFadeUpAnimationProps}
           >
-            <div className="text-4xl text-blue-600 mb-4">10%</div>
-            <p className="text-gray-600">{t("staffTurnoverRate")}</p>
+            <div className="text-4xl text-primary-blue mb-4">10%</div>
+            <p className="text-color-text">{t("staffTurnoverRate")}</p>
+          </motion.div>
+          <motion.div
+            className="text-center"
+            transition={{ ease: "easeOut", duration: 1, delay: 0.6 }}
+            {...commonFadeUpAnimationProps}
+          >
+            <div className="text-4xl text-primary-blue mb-4">20%</div>
+            <p className="text-color-text">{t("staffTurnoverRate")}</p>
           </motion.div>
         </div>
       </Container>
